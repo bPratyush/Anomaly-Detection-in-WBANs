09a3a7a44bcfe4d033a257e6bfa2d5f927024902
bPratyush <81455621+bPratyush@users.noreply.github.com>
2025-06-22 13:11:29 +0530

    create render

--- src/project/frontend/src/App.js
+++ src/project/frontend/src/App.js
@@ -457,7 +457,7 @@ Use clear formatting with headings and concise paragraphs where appropriate.`;
     formData.append("file", file);
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/upload", {
+      const res = await fetch("https://wban-anomaly-detection.onrender.com/upload", {
         method: "POST",
         body: formData,
       });
